80dc3ab57ed3156512ec77d505b2feaca7979cdf
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2025-07-05 17:40:57 -0300

    v2

--- main.ino
+++ main.ino
@@ -1,4 +1,4 @@
-  int BUT0 = A0;
+int BUT0 = A0;
 int BUT1 = A1;
 int BUT2 = A2;
 int BUT3 = A3;
@@ -10,72 +10,46 @@ int LED2 = RX;
 int LED3 = SCK;
 int LED4 = MISO;
 
-unsigned long startTime0 = 0;
-unsigned long startTime1 = 0;
-unsigned long startTime2 = 0;
-unsigned long startTime3 = 0;
-unsigned long startTime4 = 0;
+char buttonLetters[5] = {'C', 'L', 'U', 'S', 'A'};
+int buttons[5] = {BUT0, BUT1, BUT2, BUT3, BUT4};
+int leds[5] = {LED0, LED1, LED2, LED3, LED4};
 
-const unsigned long intervaloMin = 2000;
+String target = "LUCAS";
+String input = "";
 
-int dotDuration = 200;
-int dashDuration = 600;
-int symbolGap = 200;
-int letterGap = 800;
+unsigned long debounceDelay = 250;
+unsigned long lastPress[5] = {0, 0, 0, 0, 0};
 
 void setup() {
-  pinMode(BUT0, INPUT);
-  pinMode(BUT1, INPUT);
-  pinMode(BUT2, INPUT);
-  pinMode(BUT3, INPUT);
-  pinMode(BUT4, INPUT);
-
-  pinMode(LED0, OUTPUT);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-}
-
-void blinkMorseChar(char symbol, int ledPin) {
-  digitalWrite(ledPin, HIGH);
-  delay(symbol == '.' ? dotDuration : dashDuration);
-  digitalWrite(ledPin, LOW);
-  delay(symbolGap);
-}
-
-void blinkMorse(String code, int ledPin) {
-  for (int i = 0; i < code.length(); i++) {
-    blinkMorseChar(code[i], ledPin);
+  for (int i = 0; i < 5; i++) {
+    pinMode(buttons[i], INPUT);
+    pinMode(leds[i], OUTPUT);
+    digitalWrite(leds[i], LOW);
   }
-  delay(letterGap);
+}
+
+void lightAllLeds(int duration) {
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(leds[i], HIGH);
+  }
+  delay(duration);
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(leds[i], LOW);
+  }
 }
 
 void loop() {
-  unsigned long now = millis();
-
-  if (digitalRead(BUT0) == HIGH && now - startTime0 >= intervaloMin) {
-    blinkMorse(".-..", LED0);
-    startTime0 = now;
-  }
-
-  if (digitalRead(BUT1) == HIGH && now - startTime1 >= intervaloMin) {
-    blinkMorse("..-", LED1);
-    startTime1 = now;
-  }
-
-  if (digitalRead(BUT2) == HIGH && now - startTime2 >= intervaloMin) {
-    blinkMorse("-.-.", LED2);
-    startTime2 = now;
-  }
-
-  if (digitalRead(BUT3) == HIGH && now - startTime3 >= intervaloMin) {
-    blinkMorse(".-", LED3);
-    startTime3 = now;
-  }
-
-  if (digitalRead(BUT4) == HIGH && now - startTime4 >= intervaloMin) {
-    blinkMorse("...", LED4);
-    startTime4 = now;
+  for (int i = 0; i < 5; i++) {
+    if (digitalRead(buttons[i]) == HIGH && millis() - lastPress[i] > debounceDelay) {
+      input += buttonLetters[i];
+      lastPress[i] = millis();
+      if (input.length() > target.length()) {
+        input = input.substring(input.length() - target.length());
+      }
+      if (input == target) {
+        lightAllLeds(2000);
+        input = "";
+      }
+    }
   }
 }
